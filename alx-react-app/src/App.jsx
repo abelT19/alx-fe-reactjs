1ce2c1ddcd4d14585ae922673143b46d41ed6730
abelT19 <abeltariku43@gmail.com>
2025-11-02 06:57:31 -0800

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,6 @@
+
+import UserProfile from './components/UserProfile';
+
 ["<Header />", "<Footer />", "<MainContent />"]
 import Header from './components/Header';
 import MainContent from './components/MainContent';
