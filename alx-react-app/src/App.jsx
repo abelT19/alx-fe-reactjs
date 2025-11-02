f1e2715c6f11e60f7b6287f0150b9cee43b658b4
abelT19 <abeltariku43@gmail.com>
2025-11-02 06:52:56 -0800

    Import Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,13 @@
+
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+
+
+
+
+
 ["WelcomeMessage", "<WelcomeMessage />"]
 
 import { useState } from 'react'
