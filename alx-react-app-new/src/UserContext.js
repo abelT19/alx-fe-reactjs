d9a7e6dcd0102df3138428f6e554a4c5dd551f95
abelT19 <abeltariku43@gmail.com>
2025-11-09 09:48:09 -0800

    Add files via upload

---
+++ alx-react-app-new/src/UserContext.js
@@ -0,0 +1,8 @@
+// src/UserContext.js
+import { createContext } from 'react';
+
+// Create the context
+const UserContext = createContext();
+
+// Export it for use across components
+export default UserContext;
