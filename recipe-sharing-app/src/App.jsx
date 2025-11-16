b93093047457053cbc5461cb731474382ed121b6
abelT19 <abeltariku43@gmail.com>
2025-11-16 07:11:00 -0800

    Add files via upload

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,30 @@
 // File: src/App.jsx
 
-import RecipeList from "./components/RecipeList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
 
-function App() {
+function HomePage() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
       <RecipeList />
     </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+        <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
