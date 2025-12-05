1f06c991b7d45298b660e02190526c9e6b2315e6
abelT19 <abeltariku43@gmail.com>
2025-12-05 07:41:34 -0800

    Add files via upload

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RecipeDetail from "./pages/RecipeDetail";
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
